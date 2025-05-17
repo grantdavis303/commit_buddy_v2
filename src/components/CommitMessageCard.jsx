52287e58d25405a725ef5b4b78af8808d5e5262a
Grant Davis <grantdavis303@gmail.com>
2025-05-17 15:33:55 -0600

    Add CommitMessageCard Component

---
+++ src/components/CommitMessageCard.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import { Button } from '@mui/material'
+import './CommitMessageCard.css'
+
+function CommitMessageCard(commitMessage) {
+  const [copyButtonText, setCopyButtonText] = useState('Copy')
+
+  function copyToClipboard() {
+    const filePath = `git add ${commitMessage.content.filePath}`;
+    const messageContent = `git commit -m "${commitMessage.content.messageContent}"`;
+    const commands = filePath + '\n' + messageContent;
+
+    navigator.clipboard.writeText(commands);
+
+    setCopyButtonText('Copied!');
+
+    setTimeout(() => {
+      setCopyButtonText('Copy');
+    }, 2000);
+  }
+
+  return (
+    <div className='commitMessageCard'>
+      <p> git add <span className='filePath'>{commitMessage.content.filePath}</span> </p>
+      <p> git commit -m <span className='messageContent'>"{commitMessage.content.messageContent}"</span> </p>
+      <Button variant='outlined' color='white' onClick={copyToClipboard}> {copyButtonText} </Button>
+    </div>
+  )
+}
+
+export default CommitMessageCard
